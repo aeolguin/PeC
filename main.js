69953c87f0e0bb4fb3fa836c87d055c26ded4e60
aeolguin <aredolguin@gmail.com>
2019-06-27 23:12:47 -0300

    primera presentacion para Pablo

--- main.js
+++ main.js
@@ -23,73 +23,66 @@ $(function() {
         },
 
         textos = function (){
+          if (contador > 6){
+            contador = 1
+          }
           if (contador === 0) {
-            contador = contador + 1;
-            }
-            if (contador === 1) {
-              textoA = "texto1"
+            textoA = detalles.a
+          }
+          if (contador === 1) {
+              textoA = detalles.b
               textoB = ""
               textoC = ""
-            }
-            if (contador === 2) {
-              textoA = "texto2"
+          }
+          if (contador === 2) {
+              textoA = detalles.c
               textoB = ""
               textoC = ""
-            }
-            if (contador === 3) {
-              textoA = "texto3"
+          }
+          if (contador === 3) {
+              textoA = detalles.d
               textoB = ""
               textoC = ""
-            }
-            if (contador === 4) {
-              textoA = "Pablo Carrazana"
-              textoB = "Pablo@tumail.com"
-              textoC = "telefono: 11-1111-111"
-            }
-                      
-            if (contador < 5) {
+          }
+          if (contador === 4) {
+              textoA = detalles.nombre
+              textoB = detalles.mail
+              textoC = detalles.telefono
+          }
+          if (contador < 5) {
             var descripcionNav = $('.visto');
             descripcionNav.html(
               `<h2>${textoA}</h2>
               <h2>${textoB}</h2>
               <h2>${textoC}</h2>`
             );          
-            }else {
+          }else {
               contador = 0;
               var descripcionNav = $('.visto');
               descripcionNav.html(
-                `<h2>Acá iria una descripcion del servicio prestado!</h2>`
+                `<h2>${detalles.a}</h2>`
               );
-            }
+          }
         },
         initEvents = function() {
           
           // add navigation events
           $navArrows.children( ':first' ).on( 'click', function() {
-            textos();
             contador = contador + 1;
-            console.log(contador);
+            textos();
             slicebox.next();
-            
             return false;
-            
           } );
           
           $navArrows.children( ':last' ).on( 'click', function() {
-            if (contador === 0) {
-              contador = 5
-            }
-            if (contador === 1) {
-              contador = 7
-            }
             contador = contador -1;
-            console.log(contador);
+            if (contador < 0) {
+              contador = 4
+            }
             textos();
             slicebox.previous();
             return false;
-            
           } );
-          
         };
     
     return { init : init };
---
+++ textos.js
@@ -0,0 +1,9 @@
+var detalles = {
+    a : "Acá iria una descripcion del servicio prestado!",
+    b : "Soporte IT",
+    c : "Insumos",
+    d : "Control de Acceso",
+    nombre : "Pablo Carrazana",
+    mail : "Pablo@tumail.com",
+    telefono :  "telefono: 11-1111-111"
+};
